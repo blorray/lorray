a739fdf3dce02f6aa5f84c5c594b9b1d2e4ebba1
blorray <151082684+blorray@users.noreply.github.com>
2023-11-16 23:20:09 +0300

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student{
+public static void main(String[]args){
+System.out.println("Institution:KCA University");
+System.out.println("Course:DIT");
+System.out.println("Name:Loreen Wanjiku Kariuki");
+System.out.println("Reg no:22/04323");
+System.out.println("Gender:Female");
+System.out.println("DOB:17.02.2004");
+System.out.println("Phone no:+254722420334");
+System.out.println("Email Address:lorray.wk@gmail.com");
+}
+}
